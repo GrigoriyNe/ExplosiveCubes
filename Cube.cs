8cbce048c8cc2edb851807076ad4d03a0962bb21
GrigoriyNe <159809163+GrigoriyNe@users.noreply.github.com>
2024-09-04 22:05:08 +0500

    Update Cube.cs

--- Cube.cs
+++ Cube.cs
@@ -1,45 +1,40 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Cube : MonoBehaviour
 {
-    [SerializeField, Min(1)] private float _explosionRadius;
-    [SerializeField, Min(1)] private float _explosionForce;
-    [SerializeField] private ParticleSystem _effect;
-    [SerializeField] private LayerMask _explodible;
+    [SerializeField] private MeshRenderer _meshRenderer;
     [SerializeField] Cube _prefab;
-    [SerializeField] MeshRenderer _meshRenderer;
 
+    private Spawn _spawn;
+    private int _explosionForce = 10;
+    private int _explosionRadius = 10;
     private int _chanceDivision = 100;
-    private int _doubleCubesCount = 2;
-    private int _decriseValue = 2;
+
+    public int ChanceDivision => _chanceDivision;
+
+    private void Awake()
+    {
+        _spawn = GetComponent<Spawn>();
+    }
+
+    public void Initialisation(Color color, int chanceDivide, Vector3 scale)
+    {
+        _meshRenderer.material.color = color;
+        _chanceDivision = chanceDivide;
+        transform.localScale = scale;
+
+        Rigidbody rigidbody = GetComponent<Rigidbody>();
+        rigidbody.AddExplosionForce(_explosionForce, transform.position, _explosionRadius);
+    }
 
     private void OnMouseDown()
     {
         if (CanDivide())
         {
-            StartCoroutine(Create());
-            Explode();
-            Instantiate(_effect, transform.position, transform.rotation);
+            _spawn.Create(_prefab);
         }
 
-        Destroy(_effect);
         Destroy(gameObject);
-    }
-
-    private Color GetColor()
-    {
-        List<Color> colors = new List<Color>();
-        colors.Add(Color.red);
-        colors.Add(Color.blue);
-        colors.Add(Color.yellow);
-        colors.Add(Color.gray);
-        colors.Add(Color.cyan);
-
-        System.Random random = new System.Random();
-
-        return colors[random.Next(0, colors.Count)];
     }
 
     private bool CanDivide()
@@ -47,47 +42,6 @@ public class Cube : MonoBehaviour
         System.Random random = new System.Random();
         int randomValue = random.Next(0, 100);
 
-        if (randomValue < _chanceDivision)
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
-    }
-    private void Explode()
-    {
-        Collider[] colliders = Physics.OverlapSphere(transform.position, _explosionRadius, _explodible, QueryTriggerInteraction.Ignore);
-
-        foreach (Collider collider in colliders)
-
-        {
-            if (collider.TryGetComponent(out Rigidbody rigidbody) == false)
-                continue;
-
-            rigidbody.AddExplosionForce(_explosionForce, transform.position, _explosionRadius, 1);
-
-        }
-    }
-
-    private IEnumerator Create()
-    {
-        while (enabled)
-        {
-            for (int i = 0; i < _doubleCubesCount; i++)
-            {
-                Vector3 position = transform.position;
-
-                Cube cube = Instantiate(_prefab, position, Quaternion.identity);
-                Vector3 scale = cube.transform.localScale / _decriseValue;
-
-                cube.transform.localScale = scale;
-                cube._meshRenderer.material.color = GetColor();
-                cube._chanceDivision /= _decriseValue;
-            }
-
-            yield return null;
-        }
+        return randomValue < _chanceDivision;
     }
 }
