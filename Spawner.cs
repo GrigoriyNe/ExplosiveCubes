4689c7f289aa325202c9be008fd7920de7b79e0d
GrigoriyNe <159809163+GrigoriyNe@users.noreply.github.com>
2024-09-05 21:22:22 +0500

    Update and rename Spawn.cs to Spawner.cs

--- Spawner.cs
+++ Spawner.cs
@@ -1,17 +1,17 @@
 using UnityEngine;
 
-public class Spawn : MonoBehaviour
+public class Spawner : MonoBehaviour
 {
     private int _decriseValue = 2;
-    
+
     public void Create(Cube prefab)
     {
         int minValueCreateRandom = 2;
-        int maxValueCreateRandom = 7;
+        int maxValueCreateRandom = 6;
         int countCreating;
 
         System.Random random = new System.Random();
-        countCreating = random.Next(minValueCreateRandom, maxValueCreateRandom);
+        countCreating = random.Next(minValueCreateRandom, maxValueCreateRandom + 1);
 
         for (int i = 0; i < countCreating; i++)
         {
@@ -22,7 +22,7 @@ public class Spawn : MonoBehaviour
             var chanceDivide = newCube.ChanceDivision / _decriseValue;
             var scale = transform.localScale / _decriseValue;
 
-            newCube.Initialisation(color, chanceDivide, scale);
+            newCube.Init(color, chanceDivide, scale);
         }
     }
 }
